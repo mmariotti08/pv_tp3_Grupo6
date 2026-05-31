f84f3f0d9696ac83ef958dcd27875fc201943ec4
diegou777 <caridiego11@gmail.com>
2026-05-31 16:23:28 -0300

    Agrego detalle de proyecto con ruta dinamica

--- my-app/src/components/ProyectoCard.jsx
+++ my-app/src/components/ProyectoCard.jsx
@@ -1,31 +1,32 @@
 import "../css/proyectoCard.css";
+import { Link } from "react-router-dom";
 
 const ProyectoCard = ({ proyecto, alEliminar, onDetalle }) => {
-    const { id, titulo, categoria, estado } = proyecto;
+  const { id, titulo, categoria, estado } = proyecto;
 
-    return (
-        <article className="card-proyecto">
-            <h3>{titulo}</h3>
+  return (
+    <article className="card-proyecto">
+      <h3>{titulo}</h3>
 
-            <p>
-                <strong>Categoría:</strong> {categoria}
-            </p>
+      <p>
+        <strong>Categoría:</strong> {categoria}
+      </p>
 
-            <p>
-                <strong>Estado:</strong> {estado}
-            </p>
+      <p>
+        <strong>Estado:</strong> {estado}
+      </p>
 
-            <div className="botones-card">
-                <button type="button" onClick={() => onDetalle(proyecto)}>
-                    Ver detalle
-                </button>
+<div className="botones-card">
+<Link className="boton-detalle" to={`/proyectos/${id}`}>
+    Ver detalle
+</Link>
 
-                <button type="button" onClick={() => alEliminar(id)}>
-                    Eliminar
-                </button>
-            </div>
-        </article>
-    );
+<button className="boton-eliminar" type="button" onClick={() => alEliminar(id)}>
+    Eliminar
+</button>
+</div>
+    </article>
+  );
 };
 
 export default ProyectoCard;

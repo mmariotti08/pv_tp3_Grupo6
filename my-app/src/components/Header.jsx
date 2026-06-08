8bf147a2dfbe210b45ffc6a8995c9ef96ea30c46
unknown <benjaminagustinortega@gmail.com>
2026-06-08 18:01:37 -0300

    agrego Header consume datos desde UsuarioContext

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import { UsuarioContext } from "../context/UsuarioContext";
+
 const Header = () => {
+
+    const { usuario } = useContext(UsuarioContext);
     return (
         <header>
             <h1>Gestión de Proyectos Educativos</h1>
+
+            <div>
+                {usuario.nombre} | {usuario.rol}
+            </div>
         </header>
     );
 };
-
 export default Header;

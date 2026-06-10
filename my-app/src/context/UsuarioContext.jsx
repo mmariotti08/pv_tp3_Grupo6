4e82e5f3b6ff014e45e6096e3088145822d66359
Gonzalo Peñalva <gonzalomp04@gmail.com>
2026-06-09 23:32:10 -0300

    Gonzalo Peñalva (Pt1) lógica de guardado con useEffect

--- my-app/src/context/UsuarioContext.jsx
+++ my-app/src/context/UsuarioContext.jsx
@@ -1,44 +1,22 @@
-import { createContext, useState, useEffect } from 'react';
-
+import { createContext, useState, useEffect } from "react";
 
 export const UsuarioContext = createContext();
 
-
 export const UsuarioProvider = ({ children }) => {
-    
-   
-    const obtenerUsuarioInicial = () => {
-        const usuarioGuardado = localStorage.getItem("usuarioPerfil");
-        
-        
-        if (usuarioGuardado) {
-            return JSON.parse(usuarioGuardado);
-        }
-               
-        return {
-            nombre: "Matías",
-            dni: "12345678",
-            rol: "Alumno",
-            institucion: "UNJu"
-        };
+    const [usuario, setUsuario] = useState({
+        nombre: "Benjamin Ortega",
+        dni: "12345678",
+        rol: "Alumno",
+        institucion: "Facultad de Ingeniería - UNJU"
+    });
+
+    const actualizarPerfil = (nuevosDatos) => {
+        setUsuario(nuevosDatos);
     };
-
-
-    const [usuario, setUsuario] = useState(obtenerUsuarioInicial);
-
 
     useEffect(() => {
-        
-        localStorage.setItem("usuarioPerfil", JSON.stringify(usuario));
+        localStorage.setItem("usuario_session", JSON.stringify(usuario));
     }, [usuario]);
-
-  
-    const actualizarPerfil = (nuevosDatos) => {
-        setUsuario((estadoAnterior) => ({
-            ...estadoAnterior,
-            ...nuevosDatos
-        }));
-    };
 
     return (
         <UsuarioContext.Provider value={{ usuario, actualizarPerfil }}>
